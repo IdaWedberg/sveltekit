d2768efd377249ba0b4be2fb9978f0da806b8cc9
ErikKL06 <06kler@skola.englholm.se>
2024-09-06 14:53:52 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+</script>
+    
+<Header />
+
+<slot />
+
+<h1>Layout</h1>
+
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+    
+    
